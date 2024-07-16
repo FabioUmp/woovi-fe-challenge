c14c535fd08c2e15edd61853db66900c83c91f53
fabio <fabioumpierre97@gmail.com>
2024-07-15 22:08:52 -0500

    formating

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,19 +1,13 @@
-type CtaProps={
-    title:string
+type CtaProps = {
+    title: string
 }
-function Cta({
-    title
-}:CtaProps) {
 
-    return (
-  
-      <div className="text-center">
-        <p className="pt-2 text-xl font-bold text-black-500 font-medium">{title}</p>
-      </div>
-     
-    )
-  
-  }
-  
-  export default Cta
-  
+function Cta({ title }: CtaProps) {
+  return (
+    <div className="text-center">
+      <p className="pt-2 text-xl font-bold text-black-500 font-medium">{title}</p>
+    </div>
+  )
+};
+
+export default Cta;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,9 @@
-
 function Header() {
-
   return (
-
     <div className="text-center">
       <p className="pt-2 text-5xl text-green-500 font-medium">woovi</p>
     </div>
-   
   )
-
 }
 
-export default Header
+export default Header;
