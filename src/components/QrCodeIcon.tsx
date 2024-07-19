4b086e8dad8467d1bb7eaa64785b0b3c0c2be20c
fabio <fabioumpierre97@gmail.com>
2024-07-19 17:03:09 -0500

    create button for copy qr-code

--- src/components/QrCodeIcon.tsx
+++ src/components/QrCodeIcon.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
 
 const QrCodeIcon = () => {
+  const qrCodeValue = 'https://example.com';
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(qrCodeValue);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
   return (
     <div
       style={{
-        border: '2px solid #03D69D',
-        borderRadius: '10px',
-        padding: '10px',
-        display: 'inline-block',
+        textAlign: 'center',
+        marginTop: '20px',
       }}
     >
-      <QRCode value="https://example.com" size={256} />
+      <div
+        style={{
+          border: '2px solid #03D69D',
+          borderRadius: '10px',
+          padding: '10px',
+          display: 'inline-block',
+        }}
+      >
+        <QRCode value={qrCodeValue} size={256} />
+      </div>
+
+      <div style={{ marginTop: '10px' }}>
+        <button
+          style={{
+            backgroundColor: 'rgba(19, 58, 111, 255)',
+            color: 'white',
+            borderRadius: '5px',
+            fontSize: '15px',
+            width: '250px',
+            height: '30px',
+          }}
+          onClick={copyToClipboard}
+        >
+          {'Clique para copiar QR CODE'}
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Qr.tsx
+++ src/pages/Qr.tsx
@@ -6,17 +6,30 @@ import QrCodeIcon from '../components/QrCodeIcon';
 function Qr() {
   const { uuid } = useParams();
   return (
-    <div className="mx-20 mx-auto mb-5 max-w-sm flex-col rounded-3xl border-4 border-t-8 border-gray-900 bg-white px-4 py-6 text-black shadow-2xl">
+    <div className="mx-20 flex items-center justify-center mx-auto mb-5 max-w-sm flex-col rounded-3xl border-4 border-t-8 border-gray-900 bg-white px-4 py-6 text-black shadow-2xl">
       <div className="text-center">
         <Header />
         <Cta title="joao, pague a entrada pix" />
-        <p className="pt-2 text-lg font-medium">qr</p>
+        <p className="pt-2 text-lg font-medium">QR</p>
         <p className="text-sm font-extrabold text-gray-900">
-          {uuid || 'not id selected'}
+          {uuid || 'ID não selecionado'}
         </p>
       </div>
-      <div className="flex items-center justify-center mb-5">
+      <div className="text-center mt-4">
         <QrCodeIcon />
+        <div className="flex items-center justify-center mt-2 flex-col">
+          <p className="text-gray-400 text-sm font-light">
+            Prazo de pagamento:{' '}
+            <span className="font-bold text-black block">
+              15/12/2021 - 08:17
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="text-center mt-4">
+        <Link to="/credit-card" className="btn btn-info">
+          Voltar
+        </Link>
       </div>
     </div>
   );
